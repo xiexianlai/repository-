3e323853169f4c6c1d32d6e005ac6f568d9dc16d
xiexianlai <79893133@qq.com>
2019-01-14 19:52:46 +0800

    添加项目文件。

---
+++ AddTwoSum/源.asm
@@ -0,0 +1,18 @@
+;AddTwoSum.asm	Ê¾Àý
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+sum DWORD 0
+.code
+main PROC
+	mov eax,5
+	add eax,6
+	mov sum,eax
+
+	INVOKE ExitProcess,0
+main ENDP
+END main
